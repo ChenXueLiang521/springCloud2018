bc9b4b2b764148baa6a4c787fffe935bca9d810c
chenxueliang <chen.xue.liang@fesco.com.cn>
2020-06-10 17:58:44 +0800

    register center Eureka

--- microservicecloud/microservicecloud-api/src/main/java/com/xueliang/springcloud/entities/Dept.java
+++ microservicecloud/microservicecloud-api/src/main/java/com/xueliang/springcloud/entities/Dept.java
@@ -1,14 +1,14 @@
 package com.xueliang.springcloud.entities;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 
 @Data
 @Accessors(chain = true)
-@AllArgsConstructor
+@NoArgsConstructor
 public class Dept implements Serializable {
     private Long deptno;
     private String dname;
---
+++ microservicecloud/microservicecloud-eureka-7002/src/main/java/com/xueliang/springcloud/EurekaServer7002_App.java
@@ -0,0 +1,19 @@
+package com.xueliang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @description:
+ * @author: chenxueliang
+ * @date: 2020/6/10 17:47
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7002_App {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002_App.class, args);
+
+    }
+}
